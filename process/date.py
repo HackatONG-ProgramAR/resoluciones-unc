d4e1ee24c2a2b407b98c9644b50a53facaf3496e
Nano Molina <nanomolinacav@gmail.com>
2014-09-06 17:12:57 -0300

    date update

--- process/date.py
+++ process/date.py
@@ -11,8 +11,12 @@ class DateNERRunner(RegExpNERRunner):
     def __init__(self, override=False):
         day = u'<\d{1,2}>'
         of = u'<de>'
+        from_ = u'(<a><partir><del> | <del> | <por><el><lapso> | <desde><el>)'
+        to = u'(<al> | <aI> | <y><hasta><el> | <,><hasta><el>)'
         months = 'enero febrero marzo abril mayo junio julio agosto septiembre octubre noviembre diciembre'.split()
         month = options_re(months)
         year = u'<\d{4}>'
-        regexp = day + of + month + optional_re(of + year)
+        date = day + of + month + optional_re(of + year)
+        date_or_day = '(' + date + ' | ' + day + ')'
+        regexp = optional_re(from_ + date_or_day + to) + date
         super(DateNERRunner, self).__init__('date', regexp, override)
